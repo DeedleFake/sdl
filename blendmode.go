1ccf429b0a0061a12a2dce1b359efe9edbc41177
DeedleFake <yisszev@beckforce.com>
2012-07-30 19:40:58 -0400

    Finished SDL_blendmode.h.

---
+++ blendmode.go
@@ -0,0 +1,19 @@
+package sdl
+
+// #cgo pkg-config: sdl2
+//
+// #include <SDL.h>
+import "C"
+
+type BlendMode uint32
+
+func (m BlendMode) c() C.SDL_BlendMode {
+	return C.SDL_BlendMode(m)
+}
+
+const (
+	BLENDMODE_NONE  BlendMode = C.SDL_BLENDMODE_NONE
+	BLENDMODE_BLEND BlendMode = C.SDL_BLENDMODE_BLEND
+	BLENDMODE_ADD   BlendMode = C.SDL_BLENDMODE_ADD
+	BLENDMODE_MOD   BlendMode = C.SDL_BLENDMODE_MOD
+)
