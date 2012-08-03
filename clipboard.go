cc6ee995eb1bdd0379f5bf26516ae25f1a1f517b
DeedleFake <yisszev@beckforce.com>
2012-08-03 19:15:28 -0400

    Add SDL_clipboard.h.

---
+++ clipboard.go
@@ -0,0 +1,32 @@
+package sdl
+
+import (
+	"unsafe"
+)
+
+// #cgo pkg-config: sdl2
+//
+// #include <SDL.h>
+import "C"
+
+func SetClipboardText(text string) error {
+	ctext := C.CString(text)
+	defer C.free(unsafe.Pointer(ctext))
+
+	if C.SDL_SetClipboardText(ctext) != 0 {
+		return getError()
+	}
+
+	return nil
+}
+
+func GetClipboardText() string {
+	ctext := C.SDL_GetClipboardText()
+	defer C.SDL_free(unsafe.Pointer(ctext))
+
+	return C.GoString(ctext)
+}
+
+func HasClipboardText() bool {
+	return C.SDL_HasClipboardText() == C.SDL_TRUE
+}
