0a0aeff3e7b45152173bd94cd0b35fce63e88ef5
Utkan Güngördü <utkan@freeconsole.org>
2012-08-30 01:44:13 +0900

    img rw

--- image.go
+++ image.go
@@ -15,11 +15,38 @@ func Load(name string) (*Surface, error) {
 	return (*Surface)(unsafe.Pointer(s)), nil
 }
 
+func LoadRW(rw *RWops, freesrc bool) (*Surface, error) {
+	cfreesrc := C.int(0)
+	if freesrc {
+		cfreesrc = 1
+	}
+
+	s := C.IMG_Load_RW(rw.c, cfreesrc)
+	if s == nil {
+		return nil, getError()
+	}
+	return (*Surface)(unsafe.Pointer(s)), nil
+}
+
+
 func (r *Renderer) LoadTexture(name string) (*Texture, error) {
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
 	s := C.IMG_LoadTexture(r.c, cname)
+	if s == nil {
+		return nil, getError()
+	}
+	return &Texture{c: s}, nil
+}
+
+func (r *Renderer) LoadTextureRW(rw *RWops, freesrc bool) (*Texture, error) {
+	cfreesrc := C.int(0)
+	if freesrc {
+		cfreesrc = 1
+	}
+
+	s := C.IMG_LoadTexture_RW(r.c, rw.c, cfreesrc)
 	if s == nil {
 		return nil, getError()
 	}
